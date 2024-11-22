0a12e687c8871f02f636875304b3f2f6a64140a2
matheus.lacerdaaa <ra49001265863@acaosnfatima.org.br>
2024-11-22 00:43:01 -0300

    atualizado

--- frontend/src/pages/adm/usuario/login/index.jsx
+++ frontend/src/pages/adm/usuario/login/index.jsx
@@ -20,7 +20,7 @@ export default function Login() {
         "senha": senha
     }
 
-    const url = `http://localhost:3069/entrar`
+    const url = `http://4.172.207.208:5001/entrar`
     let resp = await axios.post(url, usuario);
 
     if (resp.data.token == 'undefined') {
